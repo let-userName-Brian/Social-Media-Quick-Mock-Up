dd1e04ee9770990c485057eae1594a3d7998a877
Brian <brian.hardy67@gmail.com>
2021-10-18 19:54:41 -0400

    Touched up Modals

--- src/componets/ProfileCard.js
+++ src/componets/ProfileCard.js
@@ -2,32 +2,29 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import ClickAwayListener from '@mui/material/ClickAwayListener';
-import { useState } from "react"
-import { Box } from "@mui/system"
+import { useState } from "react";
+import { Box, fontWeight } from "@mui/system";
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
 
 export default function ProfileCard({ profile }) {
 
     let birthday = new Date(profile.birthday).toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric' });
     const [open, setOpen] = useState(false);
 
-    const handleClick = () => {
-        setOpen((prev) => !prev);
-    };
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
-    const handleClickAway = () => {
-        setOpen(false);
-    };
-
-    const styles = {
+    const style = {
         position: 'absolute',
-        top: 28,
-        right: 0,
-        left: 0,
-        zIndex: 1,
-        border: '1px solid',
-        p: 1,
-        backgroundColor: 'background.paper',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
     };
 
     return (
@@ -45,18 +42,30 @@ export default function ProfileCard({ profile }) {
                     <Typography gutterBottom variant="h6" component="div">
                         Birthday: {birthday}
                     </Typography>
-                    <ClickAwayListener onClickAway={handleClickAway}>
-                        <Box sx={{ position: 'relative' }}>
-                            <button type="button" onClick={handleClick} cursor="pointer">
-                                Click me to see details
-                            </button>
-                            {open ? (
-                                <Box sx={styles}>
-                                    Hey! My favorite color is: {profile.favColor}
-                                </Box>
-                                ) : null}
-                                </Box>
-                    </ClickAwayListener>
+                    <Button onClick={handleOpen}>View Photo</Button>
+                    <Modal
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                    >
+                        <Box sx={style}>
+                        <CardMedia
+                            component="img"
+                            height="700"
+                            image={profile.profileImage}
+                            alt="user"
+                        />
+                        <Typography id="modal-modal-description" sx={{ 
+                            mt: 7, 
+                            color: 'white', 
+                            fontWeight: '600',
+                            font: 'strong'
+                            }}>
+                            Beautiful isn't it?
+                        </Typography>
+                        </Box>
+                    </Modal>
                 </CardContent>
         </Card>
     );
