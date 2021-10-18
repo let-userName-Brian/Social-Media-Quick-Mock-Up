dc8fdab4e031e7ae104c384900cba90b95c89614
Brian <brian.hardy67@gmail.com>
2021-10-18 16:58:29 -0400

    added click on contacts

--- src/componets/ProfileCard.js
+++ src/componets/ProfileCard.js
@@ -1,29 +1,68 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import CardActionArea from '@mui/material/CardActionArea'
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+import { useState } from "react"
+import { Box } from "@mui/system"
 
 export default function ProfileCard({ profile }) {
 
-    let birthday = new Date(profile.birthday).toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric' })
+    let birthday = new Date(profile.birthday).toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric' });
+    const [open, setOpen] = useState(false);
+
+    const handleClick = () => {
+        setOpen((prev) => !prev);
+    };
+
+    const handleClickAway = () => {
+        setOpen(false);
+    };
+
+    const styles = {
+        position: 'absolute',
+        top: 28,
+        right: 0,
+        left: 0,
+        zIndex: 1,
+        border: '1px solid',
+        p: 1,
+        bgcolor: 'background.paper',
+    };
+    
 
     return (
-        <Card sx={{ width: 345, m: 4 }}>
-            <CardMedia
-                component="img"
-                height="300"
-                image={profile.profileImage}
-                alt="user"
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    {profile.firstName} {profile.lastName}
-                </Typography>
-                <Typography gutterBottom variant="h6" component="div">
-                    Birthday: {birthday}
-                </Typography>
-            </CardContent>
+        <Card sx={{ width: 345, m: 4, height: 460 }}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="300"
+                    image={profile.profileImage}
+                    alt="user"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {profile.firstName} {profile.lastName}
+                    </Typography>
+                    <Typography gutterBottom variant="h6" component="div">
+                        Birthday: {birthday}
+                    </Typography>
+                    <ClickAwayListener onClickAway={handleClickAway}>
+                                <Box sx={{ position: 'relative' }}>
+                                <button type="button" onClick={handleClick}>
+                                    Details
+                                </button>
+                                {open ? (
+                                <Box sx={styles}>
+                                    Hey! My favorite color is: {profile.favColor}
+                                </Box>
+                                ) : null}
+                                </Box>
+                            </ClickAwayListener>
+                </CardContent>
+            </CardActionArea>
         </Card>
     );
 }
--- src/componets/Profiles.js
+++ src/componets/Profiles.js
@@ -3,8 +3,31 @@ import ProfileCard from "./ProfileCard"
 import { Box } from "@mui/system"
 import { Typography } from "@mui/material"
 import image from "./image.png"
+import { useState } from "react"
+import ClickAwayListener from '@mui/material/ClickAwayListener';
 
 export default function Profiles() {
+    const [open, setOpen] = useState(false);
+
+    const handleClick = () => {
+        setOpen((prev) => !prev);
+    };
+
+    const handleClickAway = () => {
+        setOpen(false);
+    };
+
+    const styles = {
+        position: 'absolute',
+        top: 28,
+        right: 0,
+        left: 0,
+        zIndex: 1,
+        border: '1px solid',
+        p: 1,
+        bgcolor: 'background.paper',
+    };
+    
     return (
         <div>
             <Box sx={{ p: 5,
@@ -14,7 +37,7 @@ export default function Profiles() {
             }}>
                 <Typography variant="h3" color='white'>Contacts</Typography>
                 <Box display="flex" flexWrap="wrap" background-color='text.secondary'>
-                    {UserProfiles.userProfiles.map((profile, index) => <ProfileCard profile={profile} />)}
+                    {UserProfiles.userProfiles.map((profile, index) => <ProfileCard profile={profile}/>)}
                 </Box>
             </Box>
         </div>
